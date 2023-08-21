26e904860d6928e0bc026de9afb0e8680622018a
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2023-08-21 12:17:28 -0400

    ultimos cambios

--- assets/js/4_parte_2.js
+++ assets/js/4_parte_2.js
@@ -1,19 +1,20 @@
 //Ejercicio 4 (Parte 2)
 
 const colorDiv = document.getElementById("key");
+const colorDiv2 = document.getElementById("key2");
 
 document.addEventListener("keydown", function (event) {
     if (event.key === "a") {
         colorDiv.style.backgroundColor = "pink";
     } else if (event.key === "s") {
-        colorDiv.style.backgroundColor = "orange";
+        colorDiv.style.backgroundColor = "coral";
     } else if (event.key === "d") {
         colorDiv.style.backgroundColor = "lightblue";
     } else if (event.key === "q") {
-        colorDiv.style.backgroundColor = "purple";
+        colorDiv2.style.backgroundColor = "purple";
     } else if (event.key === "w") {
-        colorDiv.style.backgroundColor = "grey";
+        colorDiv2.style.backgroundColor = "grey";
     } else if (event.key === "e") {
-        colorDiv.style.backgroundColor = "brown";
+        colorDiv2.style.backgroundColor = "sienna";
     }
 });
